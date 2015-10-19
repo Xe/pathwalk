d96cc55b51769ae8c2b69caaba48f17e1c38526d
Chad Austin <chad@chadaustin.me>
2015-10-19 08:24:04 -0700

    document pathWalk

--- src/System/Directory/PathWalk.hs
+++ src/System/Directory/PathWalk.hs
@@ -7,6 +7,17 @@ import Data.IORef (newIORef, readIORef, writeIORef)
 import System.Directory (doesDirectoryExist, doesFileExist, getDirectoryContents)
 import System.FilePath ((</>))
 
+-- | 'pathWalk' recursively enumerates the given root directory,
+-- calling callback once per directory with the traversed directory name, a list of subdirectories, and a list of files.
+--
+-- The subdirectories and file names are always relative to the root given.
+--
+-- @
+-- pathWalk "src" $ \\dir subdirs files -> do
+--   forM_ files $ \\file -> do
+--     when ("Test.hs" \`isSuffixOf\` file) $ do
+--       registerTestFile $ dir \</\> file
+-- @
 pathWalk :: FilePath -> (FilePath -> [FilePath] -> [FilePath] -> IO ()) -> IO ()
 pathWalk root callback = do
     names <- getDirectoryContents root
